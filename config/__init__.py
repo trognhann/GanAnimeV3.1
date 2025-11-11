effae0ad0e6a1d9ae87ee5a01646ad2a0c98ca10
Trognhann <trognhann@Trognhanns-MacBook-Air.local>
2025-11-11 16:21:29 +0700

    salt Noise

--- config/__init__.py
+++ config/__init__.py
@@ -10,3 +10,5 @@ with open('./config/config.yml', 'r') as f:
 DATASET = config['DATASET']
 DATA_TEMP = config['DATA_TEMP']
 DATA_ANIME = config['DATA_ANIME']
+DATASET_SMOOTH = config['DATASET_SMOOTH']
+DATASET_SMOOTH_NOISE = config['DATASET_SMOOTH_NOISE']
